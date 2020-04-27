c0cc8d3dc7d4b8eb87fd181aaa2cf10ea69ff95e
lbugnion <laurent@galasoft.ch>
2020-04-27 14:32:58 +0200

    A few changes for test and debug

--- WordsOfTheDayApp/WordsOfTheDayApp/Model/Constants.cs
+++ WordsOfTheDayApp/WordsOfTheDayApp/Model/Constants.cs
@@ -4,6 +4,11 @@
     {
         public const string AzureWebJobsStorage = "AzureWebJobsStorage";
         public const string KeywordsBlob = "keywords.json";
+
+#if DEBUG
+        public const string QueueName = "test-markdown-to-process";
+#else
         public const string QueueName = "markdown-to-process";
+#endif
     }
 }
--- WordsOfTheDayApp/WordsOfTheDayApp/ReplaceKeywords.cs
+++ WordsOfTheDayApp/WordsOfTheDayApp/ReplaceKeywords.cs
@@ -5,6 +5,7 @@ using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 using WordsOfTheDayApp.Model;
