55779cf73fab4a5bfa5462694373a4ca89d8b59c
lbugnion <laurent@galasoft.ch>
2020-06-26 20:50:22 +0200

    New video format

--- WordsOfTheDayApp/WordsOfTheDayApp/Model/TopicMaker.cs
+++ WordsOfTheDayApp/WordsOfTheDayApp/Model/TopicMaker.cs
@@ -27,7 +27,7 @@ namespace WordsOfTheDayApp.Model
         private const string DownloadLinkTemplate = "https://wordsoftheday.blob.core.windows.net/videos/{0}.{1}.mp4";
         private const string H1 = "# ";
         private const string KeywordsMarker = "> Keywords: ";
-        private const string YouTubeEmbed = "<iframe width=\"560\" height=\"560\" src=\"https://www.youtube.com/embed/{0}\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>";
+        private const string YouTubeEmbed = "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/{0}\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>";
         private const string YouTubeEmbedMarker = "<!-- YOUTUBEEMBED -->";
         private const string YouTubeMarker = "> YouTube: ";
         private const string DownloadTarget = "<a id=\"download\"></a>";
