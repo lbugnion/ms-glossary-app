a306ab3c02d0740d803702c9a5c0129b197432c1
lbugnion <laurent@galasoft.ch>
2021-01-03 13:42:52 +0100

    Loading Synopsis from local JSON file for tests.
    Saving Synopsis in Dependency injection

---
+++ SynopsisClient/SynopsisClient/Model/SynopsisHandler.cs
@@ -0,0 +1,27 @@
+﻿using MsGlossaryApp.DataModel;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace SynopsisClient.Model
+{
+    public class SynopsisHandler
+    {
+        private static Term Synopsis
+        {
+            get;
+            set;
+        }
+
+        public async Task<Term> GetSynopsis()
+        {
+            if (Synopsis == null)
+            {
+                var client = new HttpClient();
+                Synopsis = await client.GetFromJsonAsync<Term>("https://localhost:44395/sample-data/test-topic-15.json");
+            }
+
+            return Synopsis;
+        }
+    }
+}
