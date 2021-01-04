0e99cf110125326f92d4f93aa1158904f89ef0da
lbugnion <laurent@galasoft.ch>
2021-01-04 11:30:07 +0100

    Experimenting with Blazor, data objects, validation

---
+++ _Experiments/Blazor-Experiments/BlazorApp3/Pages/CodeBehind.razor.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp3.Pages
+{
+    public partial class CodeBehind
+    {
+        private EditContext _editContext;
+        private Example _example = new Example
+        {
+            Name = "Laurent Bugnion"
+        };
+
+        private void HandleValidSubmit()
+        {
+            Console.WriteLine("Submitted");
+        }
+
+        protected override void OnInitialized()
+        {
+            _editContext = new EditContext(_example);
+            _editContext.OnFieldChanged += EditContextOnFieldChanged;
+        }
+
+        private void EditContextOnFieldChanged(object sender, FieldChangedEventArgs args)
+        {
+            Console.WriteLine("Field has changed " + args.FieldIdentifier.FieldName);
+
+            var example = (Example)args.FieldIdentifier.Model;
+
+            Console.WriteLine("New value: " + example.Name);
+        }
+
+        public class Example
+        {
+            [Required]
+            [StringLength(10, ErrorMessage = "Name is too long.")]
+            public string Name
+            {
+                get;
+                set;
+            }
+        }
+    }
+}
