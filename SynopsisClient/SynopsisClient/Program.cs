3690ba1446d6ced8d14240d301d20d76ad895341
LBugnion <lbugnion@outlook.com>
2021-01-23 13:10:00 +0100

    Versioning to 0.2

--- SynopsisClient/SynopsisClient/Program.cs
+++ SynopsisClient/SynopsisClient/Program.cs
@@ -10,7 +10,7 @@ using System.Reflection;
 using System.Threading.Tasks;
 
 // Set version number for the assembly.
-[assembly: AssemblyVersionAttribute("0.1.*")]
+[assembly: AssemblyVersionAttribute("0.2.*")]
 
 namespace SynopsisClient
 {
