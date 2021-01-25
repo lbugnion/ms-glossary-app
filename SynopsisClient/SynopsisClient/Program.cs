92af91cbb67d538e9fc0f4a6a4e50fb153546506
LBugnion <lbugnion@outlook.com>
2021-01-25 14:58:33 +0100

    Versioned Synopsis Client to V1.1

--- SynopsisClient/SynopsisClient/Program.cs
+++ SynopsisClient/SynopsisClient/Program.cs
@@ -10,7 +10,7 @@ using System.Reflection;
 using System.Threading.Tasks;
 
 // Set version number for the assembly.
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1.*")]
 
 namespace SynopsisClient
 {
