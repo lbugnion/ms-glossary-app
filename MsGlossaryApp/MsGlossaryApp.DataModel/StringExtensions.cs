429ad322c98d43e7d27c8032368afee613c59251
LBugnion <lbugnion@outlook.com>
2021-01-25 20:08:23 +0100

    Added hash to special characters

--- MsGlossaryApp/MsGlossaryApp.DataModel/StringExtensions.cs
+++ MsGlossaryApp/MsGlossaryApp.DataModel/StringExtensions.cs
@@ -122,6 +122,7 @@ namespace MsGlossaryApp.DataModel
                 .Replace('\'', '-')
                 .Replace(',', '-')
                 .Replace('\\', '-')
+                .Replace('#', '-')
                 .Replace("---", "-");
         }
 
