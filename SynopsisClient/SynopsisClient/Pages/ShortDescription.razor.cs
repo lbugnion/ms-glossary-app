24a25eee43557a70a992df8ef6a877ebb3112aad
LBugnion <lbugnion@outlook.com>
2021-01-28 13:14:58 +0100

    Enhancement # 18: Add a dynamic character count for Short Description

--- SynopsisClient/SynopsisClient/Pages/ShortDescription.razor.cs
+++ SynopsisClient/SynopsisClient/Pages/ShortDescription.razor.cs
@@ -1,12 +1,20 @@
 ﻿using Blazored.Modal.Services;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.Extensions.Logging;
+using MsGlossaryApp.DataModel;
+using SynopsisClient.Model;
+using System;
 using System.Threading.Tasks;
 
 namespace SynopsisClient.Pages
 {
-    public partial class ShortDescription
+    public partial class ShortDescription : IDisposable
     {
+        private int _characters;
+        private string _charInfoClass = ClientConstants.Css.WordsInfoGoodClass;
+        private string _charSpanClass = ClientConstants.Css.WordsCountGoodClass;
+
         [CascadingParameter]
         private IModalService Modal
         {
@@ -43,7 +51,61 @@ namespace SynopsisClient.Pages
                 Nav.NavigateTo("/");
             }
 
+            CountCharacters();
+            Handler.CurrentEditContext.OnValidationStateChanged
+                += CurrentEditContextOnValidationStateChanged;
+
             Log.LogInformation("OnInitializedAsync ->");
+        }
+
+        public void Dispose()
+        {
+            Handler.CurrentEditContext.OnValidationStateChanged
+                -= CurrentEditContextOnValidationStateChanged;
+        }
+
+        private void CountCharacters()
+        {
+            _characters = Handler.Synopsis.ShortDescription.Length;
+
+            if (_characters < Constants.MinCharactersInDescription
+                || _characters > Constants.MaxCharactersInDescription)
+            {
+                _charInfoClass = ClientConstants.Css.WordsInfoBadClass;
+                _charSpanClass = ClientConstants.Css.WordsCountBadClass;
+            }
+            else
+            {
+                _charInfoClass = ClientConstants.Css.WordsInfoGoodClass;
+                _charSpanClass = ClientConstants.Css.WordsCountGoodClass;
+            }
+
+            Log.LogDebug($"HIGHLIGHT--{_characters} characters");
+
+            StateHasChanged();
+        }
+
+        private void CurrentEditContextOnValidationStateChanged(
+            object sender,
+            ValidationStateChangedEventArgs e)
+        {
+            CountCharacters();
+        }
+
+        private async Task ReloadFromCloud()
+        {
+            Log.LogInformation("-> ReloadFromCloud");
+
+            Handler.CurrentEditContext.OnValidationStateChanged
+                -= CurrentEditContextOnValidationStateChanged;
+
+            await Handler.ReloadFromCloud();
+
+            Handler.CurrentEditContext.OnValidationStateChanged
+                += CurrentEditContextOnValidationStateChanged;
+
+            CountCharacters();
+            Log.LogInformation("ReloadFromCloud ->");
         }
     }
 }
