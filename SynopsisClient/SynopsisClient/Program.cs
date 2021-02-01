cc1264c8ee54e5c951759a7564a21e3dbce798db
LBugnion <lbugnion@outlook.com>
2021-02-01 10:35:20 +0100

    V1.6

--- SynopsisClient/SynopsisClient/Program.cs
+++ SynopsisClient/SynopsisClient/Program.cs
@@ -10,7 +10,7 @@ using System.Reflection;
 using System.Threading.Tasks;
 
 // Set version number for the assembly.
-[assembly: AssemblyVersion("1.5.*")]
+[assembly: AssemblyVersion("1.6.*")]
 
 namespace SynopsisClient
 {
